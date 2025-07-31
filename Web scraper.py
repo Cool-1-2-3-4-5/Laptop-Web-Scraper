1212afd3a6c9a0b75bd0db544a95f2d88c2f9f06
Elil Thirumugam <elilgrade8@gmail.com>
2025-07-30 22:22:38 -0400

    intial

--- Web scraper.py
+++ Web scraper.py
@@ -1,4 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome()
-driver.get("https://www.google.com")
+from selenium.webdriver.chrome.service import Service
+import time
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
+service = Service(PATH)
+driver = webdriver.Chrome(service=service)
+driver.get("https://www.bestbuy.ca/en-ca")
+
+time.sleep(3)
+enter = driver.find_element("name", "search")
+enter.send_keys("Engineering Laptops")
+enter.send_keys(Keys.RETURN)
+time.sleep(10)
