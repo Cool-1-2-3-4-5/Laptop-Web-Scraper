2e757194acf28a5c855d518c9345d012142a3435
Elil Thirumugam <elilgrade8@gmail.com>
2025-09-03 19:24:53 -0400

    good

--- Web scraper.py
+++ Web scraper.py
@@ -5,6 +5,28 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from google.oauth2.service_account import Credentials
+import gspread
+
+# Define the scope
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Path to your service account JSON key file
+SERVICE_ACCOUNT_FILE = r"c:\Users\elilt\OneDrive\Desktop\Projects\laptop-web-scraper-data-0cc293608455.json"
+
+# Authenticate and create the client
+creds = Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE,
+    scopes=scope
+)
+gc = gspread.authorize(creds)
+
+# Open the Google Sheet
+spreadsheet = gc.open("Web Scraper")
+worksheet = spreadsheet.sheet1
 
 PATH = r"C:\Users\elilt\OneDrive\Desktop\Projects\Laptop-Web-Scraper\chromedriver-win64\chromedriver.exe"
 service = Service(PATH)
@@ -65,6 +87,42 @@ time.sleep(0.5)
 state = driver.find_element(By.ID, "facetFilter-ProductCondition_BrandNew")
 time.sleep(0.5)
 driver.execute_script("arguments[0].click();", state)
-time.sleep(20)
+time.sleep(6)
 
-print("Good!")
+LaptopNamesRaw = driver.find_elements(By.CLASS_NAME, "productItemName_3IZ3c")
+# PricesRaw = driver.find_elements(By.CLASS_NAME, "productPricingContainer_3gTS3")
+# SavingRaw = driver.find_elements(By.CLASS_NAME, "style-module_productSaving__g7g1G style-module_right__Dvbyx")
+# ReviewsRaw = driver.find_elements(By.CSS_SELECTOR, 'span[data-automation="rating-count"]')
+LaptopNames = []
+# Prices = []
+# Savings = []
+# TruePrices = []
+# Reviews = []
+# print(len(LaptopNamesRaw))
+# print("Good1")
+# print(len(PricesRaw))
+# print(PricesRaw)
+# print("Good2")
+# print(len(SavingRaw))
+# print(SavingRaw)
+# print("Good3")
+# print(len(TruePrices))
+
+# print("Good4")
+# print(len(ReviewsRaw))
+# print(ReviewsRaw)
+# print("Good5")
+for num in range(0,len(LaptopNamesRaw)):
+    LaptopNames.append(LaptopNamesRaw[num].text)
+#     Prices.append(PricesRaw[num].text)
+#     x = int(PricesRaw[num].replace("$",''))
+#     Savings.append(SavingRaw[num].text)
+#     y = int(Savings[num].replace("$",''))
+#     TruePrices.append(x+y)
+#     Reviews.append(Reviews[num].text)
+print(LaptopNames)
+# print("Good!")
+# style-module_productSaving__g7g1G style-module_right__Dvbyx
+# style-module_productSaving__g7g1G style-module_right__Dvbyx
+# style-module_screenReaderOnly__4QmbS style-module_large__g5jIz
+# style-module_screenReaderOnly__4QmbS style-module_large__g5jIz
